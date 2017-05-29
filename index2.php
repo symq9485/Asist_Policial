18117a5b16714bab6cb7fde8abc5d2d970c4d4b2
Samy Mahmod <symq9485@gmail.com>
2017-05-29 10:00:44 -0400

    Comensando las mejoras para sistemas_2

---
+++ index2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <header name="Cabecera" id="Cabecera">
+
+    </header>
+    <nav>
+
+    </nav>
+
+    <div>
+
+    </div>
+
+    <footer>
+
+    </footer>
+  </body>
+</html>
